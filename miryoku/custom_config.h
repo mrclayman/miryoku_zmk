cdb08ec94676cda21b99f551cacae8795283bb8b
Zbynek Novotny <znovotny@gmail.com>
2024-10-27 20:43:01 +0100

    Added modtap customization for easy access to Czech accents

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             U_MT(LS(RA(N0)), B),  U_MT(RS(RA(N0)), J),  &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    U_MT(LS(RA(DOT)), G), U_MT(RS(RA(DOT)), M), U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             U_MT(RA(SQT), V),     U_MT(RA(SQT), K),     &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),   U_LT(U_SYM, RET),     U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
